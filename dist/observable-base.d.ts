e05040925209eb19bc1b1b34f19f735cc2f313b4
electricessence <5899455+electricessence@users.noreply.github.com>
2022-02-12 12:48:55 -0800

    Export missing interfaces.

--- dist/observable-base.d.ts
+++ dist/observable-base.d.ts
@@ -2,7 +2,9 @@
  * @author electricessence / https://github.com/electricessence/
  * @license MIT
  */
+import Observer from './Observer';
+import Observable from './Observable';
 import ObservableBase from './ObservableBase';
 import ObservableNodeBase from './ObservableNodeBase';
 import SubscribableBase from './SubscribableBase';
-export { ObservableBase, ObservableNodeBase, SubscribableBase };
+export { Observer, Observable, ObservableBase, ObservableNodeBase, SubscribableBase };
--- src/observable-base.ts
+++ src/observable-base.ts
@@ -3,11 +3,16 @@
  * @license MIT
  */
 
+
+import Observer from './Observer';
+import Observable from './Observable';
 import ObservableBase from './ObservableBase';
 import ObservableNodeBase from './ObservableNodeBase';
 import SubscribableBase from './SubscribableBase';
 
 export {
+	Observer,
+	Observable,
 	ObservableBase,
 	ObservableNodeBase,
 	SubscribableBase
